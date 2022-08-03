f11ed68035bb9c31b6b19f098af4bc65cb44c907
MerveDur <merve__dur@hotmail.com>
2022-08-03 22:42:39 +0300

    update porgram.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
 using System.Collections;
 #nullable disable // String null olabilir uyarısı engelleme
 
@@ -9,6 +9,14 @@ namespace HelloWorld
     {
         public void getMenu()
         {
+            Console.WriteLine();
+            Console.WriteLine("Lütfen yapmak istediğiniz işlemi seçiniz :) ");
+            Console.WriteLine("*******************************************");
+            Console.WriteLine("(1) Board Listelemek");
+            Console.WriteLine("(2) Board'a Kart Eklemek");
+            Console.WriteLine("(3) Board'dan Kart Silmek");
+            Console.WriteLine("(4) Kart Taşımak");
+            Console.WriteLine("(5) Çıkış Yapmak");
         }
     }
 }
