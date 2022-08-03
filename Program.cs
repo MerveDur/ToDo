59cf7a8c722bf72acefea2545ecb3c91d9bd6532
MerveDur <merve__dur@hotmail.com>
2022-08-03 18:33:10 +0300

    create project

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+using System.Collections;
+#nullable disable // String null olabilir uyarısı engelleme
+
+namespace HelloWorld
+{
+    class Menu
+    {
+        public void getMenu()
+        {
+        }
+    }
+}
